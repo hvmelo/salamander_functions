c7139c2420c95479cb71063c28aa61413811a1ee
Henrique Velloso <hvmelo@gmail.com>
2022-01-26 02:36:06 -0300

    Update index.js
    - Added environment variables for lnd host and port.
    - Created method activateLightning, which will be used for each functions that wants to access the lnd server

--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,16 @@
 
 import * as functions from "firebase-functions";
 import admin from "firebase-admin";
-import {HttpsError} from "firebase-functions/v1/https";
+import { HttpsError } from "firebase-functions/v1/https";
 import LndGrpc from "lnd-grpc";
-import {SecretManagerServiceClient} from "@google-cloud/secret-manager";
-
+import { SecretManagerServiceClient } from "@google-cloud/secret-manager"
 // Initialize Firestore
 admin.initializeApp();
 const walletsCollection = admin.firestore().collection("wallets");
 const usersCollection = admin.firestore().collection("users");
 
-// Initialize the Secret Manager
-const project = "projects/salamander-cloud";
+// Secret Manager paths
+const project = `projects/${process.env.GCLOUD_PROJECT}`;
 const MACAROON_RESOURCE_NAME =
   `${project}/secrets/lnd-server-admin-macaroon/versions/latest`;
 const PASSWORD_RESOURCE_NAME =
@@ -20,139 +19,166 @@ const PASSWORD_RESOURCE_NAME =
 const TLS_CERT_RESOURCE_NAME =
   `${project}/secrets/lnd-server-tls-cert/versions/latest`;
 
-
-// Initialize LND client
-const LND_HOST = "salamanderlnd.ddns.net";
-const LND_PORT = 10009;
-
-// Declare the grpc (will be lazy initialized)
+// Lnd server configuration
+const LND_HOST = functions.config().lnd.host;
+const LND_PORT = functions.config().lnd.port;
+// Declare the grpc (will be lazily initialized)
 let grpc;
 let walletPassword;
 
 export const createWallet = functions.
-    https.onCall((data, context) => {
+  https.onCall((data, context) => {
     // verify Firebase Auth ID token
-      if (!context.auth) {
-        return {message: "Authentication Required!", code: 401};
-      }
+    if (!context.auth) {
+      throw new HttpsError("authentication-error", "Authetication required!");
+    }
 
-      const userId = context.auth.uid;
-      const walletDoc = {
-        "current_balance": 0,
-        "last_updated": new Date(),
-        "owner_id": userId,
-      };
+    // // Connect to the lnd server and activate lightining (if needed)
+    // await activateLightning();
 
-      const walletDocRef = walletsCollection.doc();
+    // if (grpc.state == "active") {
+    //   const { Lightning } = grpc.services;
+    //   const balance = await Lightning.walletBalance();
+    // } 
+    // else 
+    // {
+    //   throw new HttpsError("wallet-balance-error",
+    //     "Error when trying to activate lightning");
+    // }
 
-      const userDoc = {
-        "current_wallet_id": walletDocRef.id,
-      };
 
-      const batch = admin.firestore().batch();
-      batch.set(walletDocRef, walletDoc);
-      batch.set(usersCollection.doc(userId), userDoc);
+    const userId = context.auth.uid;
+    const walletDoc = {
+      "current_balance": 0,
+      "last_updated": new Date(),
+      "owner_id": userId,
+    };
 
-      return batch.commit()
-          .then((writeResult) => {
-            console.log(writeResult);
-            return {wallet_id: walletDocRef.id};
-          })
-          .catch((err) => {
-            console.log(err);
-            throw new HttpsError("write-wallet-error",
-                "Error while writing to the database");
-          });
-    });
+    const walletDocRef = walletsCollection.doc();
+
+    const userDoc = {
+      "current_wallet_id": walletDocRef.id,
+    };
+
+    const batch = admin.firestore().batch();
+    batch.set(walletDocRef, walletDoc);
+    batch.set(usersCollection.doc(userId), userDoc);
+
+    return batch.commit()
+      .then((writeResult) => {
+        console.log(writeResult);
+        return { wallet_id: walletDocRef.id };
+      })
+      .catch((err) => {
+        console.log(err);
+        throw new HttpsError("write-wallet-error",
+          "Error while writing to the database");
+      });
+  });
 
 // Saves a message to the Firebase Realtime Database but sanitizes the
 // text by removing swearwords.
 export const addMessage = functions.https.
-    onCall((data, context) => {
+  onCall((data, context) => {
     // Message text passed from the client.
-      const text = data.text;
+    const text = data.text;
 
-      // Checking attribute.
-      if (!(typeof text === "string") || text.length === 0) {
+    // Checking attribute.
+    if (!(typeof text === "string") || text.length === 0) {
       // Throwing an HttpsError so that the client gets the error details.
-        throw new HttpsError("invalid-argument",
-            "The function must be called with one arguments \"text\" " +
+      throw new HttpsError("invalid-argument",
+        "The function must be called with one arguments \"text\" " +
         "containing the message text to add.");
-      }
-      // Checking that the user is authenticated.
-      if (!context.auth) {
+    }
+    // Checking that the user is authenticated.
+    if (!context.auth) {
       // Throwing an HttpsError so that the client gets the error details.
-        throw new HttpsError("failed-precondition",
-            "The function must be called while authenticated.");
-      }
+      throw new HttpsError("failed-precondition",
+        "The function must be called while authenticated.");
+    }
 
-      // Authentication / user information is automatically added to the
-      // request.
-      const uid = context.auth.uid;
-      const name = context.auth.token.name || null;
-      const picture = context.auth.token.picture || null;
-      const email = context.auth.token.email || null;
+    // Authentication / user information is automatically added to the
+    // request.
+    const uid = context.auth.uid;
+    const name = context.auth.token.name || null;
+    const picture = context.auth.token.picture || null;
+    const email = context.auth.token.email || null;
 
-      // Saving the new message to Firestore.
-      return admin.firestore().collection("messages").add({
-        text: text.toUpperCase(),
-        author: {uid, name, picture, email},
-      }).then(() => {
-        console.log("New Message written");
-        // Returning the sanitized message to the client.
-        return {text: text.toUpperCase()};
-      })
-          .catch((error) => {
-            // Re-throwing the error as an HttpsError so that the client
-            // gets the error details.
-            throw new HttpsError("unknown", error.message, error);
-          });
-    });
+    // Saving the new message to Firestore.
+    return admin.firestore().collection("messages").add({
+      text: text.toUpperCase(),
+      author: { uid, name, picture, email },
+    }).then(() => {
+      console.log("New Message written");
+      // Returning the sanitized message to the client.
+      return { text: text.toUpperCase() };
+    })
+      .catch((error) => {
+        // Re-throwing the error as an HttpsError so that the client
+        // gets the error details.
+        throw new HttpsError("unknown", error.message, error);
+      });
+  });
 
 
 export const walletBalance = functions.
-    https.onCall(async (data, context) => {
+  https.onCall(async (data, context) => {
     // verify Firebase Auth ID token
-      if (!context.auth) {
-        return {message: "Authentication Required!", code: 401};
-      }
+    if (!context.auth) {
+      return { message: "Authentication Required!", code: 401 };
+    }
 
-      if (grpc == null) {
-        grpc = await connectToServer();
-      }
+    // Connect to the lnd server and activate lightining (if needed)
+    await activateLightning();
 
-      if (grpc.state != "active") {
-        await grpc.connect();
-        if (grpc.state == "ready") {
-          throw new HttpsError("server-offline", "Can't connect to LND server");
-        }
-      }
+    if (grpc.state == "active") {
+      const { Lightning } = grpc.services;
+      const balance = await Lightning.walletBalance();
+      return { wallet_balance: balance };
+    }
+    else {
+      throw new HttpsError("wallet-balance-error",
+        "Error when trying to activate lightning");
+    }
+  });
 
-      if (grpc.state === "locked") {
-        const {WalletUnlocker} = grpc.services;
-        if (walletPassword == null) {
-          walletPassword = await getSecret(PASSWORD_RESOURCE_NAME);
-        }
-        await WalletUnlocker.unlockWallet({
-          wallet_password: walletPassword,
-        });
-        if (grpc.state != "locked") {
-          await grpc.activateLightning();
-        } else {
-          throw new HttpsError("wrong-password", "Can't connect to LND server");
-        }
-      }
+/**
+ * Connect to the LND server and activate lightning
+ */
+async function activateLightning() {
+  if (grpc == null) {
+    const macaroon = await getSecret(MACAROON_RESOURCE_NAME);
+    const cert = await getSecret(TLS_CERT_RESOURCE_NAME);
 
-      if (grpc.state == "active") {
-        const {Lightning} = grpc.services;
-        const balance = await Lightning.walletBalance();
-        console.log(balance);
-        return {wallet_balance: balance};
-      } else {
-        throw new HttpsError("wallet-balance-error",
-            "Error while trying to connect to the LND server");
-      }
+    grpc = new LndGrpc({
+      host: `${LND_HOST}:${LND_PORT}`,
+      cert: cert.toString(),
+      macaroon: macaroon.toString("hex"),
     });
+  }
+
+  if (grpc.state != "active") {
+    await grpc.connect();
+    if (grpc.state == "ready") {
+      throw new HttpsError("server-offline", "Can't connect to the LND server");
+    }
+  }
+
+  if (grpc.state === "locked") {
+    const { WalletUnlocker } = grpc.services;
+    if (walletPassword == null) {
+      walletPassword = await getSecret(PASSWORD_RESOURCE_NAME);
+    }
+    await WalletUnlocker.unlockWallet({
+      wallet_password: walletPassword,
+    });
+    if (grpc.state != "locked") {
+      await grpc.activateLightning();
+    } else {
+      throw new HttpsError("wrong-password", "Can't connect to LND server");
+    }
+  }
+}
 
 /**
  * Retrieves a secret.
@@ -163,21 +189,6 @@ async function getSecret(resourceName) {
   // Access the secret.
   const secretClient = new SecretManagerServiceClient();
   const [version] =
-    await secretClient.accessSecretVersion({name: resourceName});
+    await secretClient.accessSecretVersion({ name: resourceName });
   return version.payload.data;
-}
-
-/**
- * Connects to the LND server.
- * @return {grpc} The grpc connection object.
- */
-async function connectToServer() {
-  const macaroon = await getSecret(MACAROON_RESOURCE_NAME);
-  const cert = await getSecret(TLS_CERT_RESOURCE_NAME);
-
-  return new LndGrpc({
-    host: `${LND_HOST}:${LND_PORT}`,
-    cert: cert.toString(),
-    macaroon: macaroon.toString("hex"),
-  });
 }
