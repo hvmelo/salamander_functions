14a41a7fde4146f4273da88371e9bf4a6f2e5d2e
Henrique Velloso <hvmelo@gmail.com>
2022-03-23 15:34:31 -0300

    Refactored earliest/latest block logic in SyncTransactions

--- functions/functions/sync_transactions.js
+++ functions/functions/sync_transactions.js
@@ -23,7 +23,7 @@ export const syncTransactions = functions.
 
       if (addresses.length == 0 && wallets.length == 0) {
         console.log("Both addresses and wallets collections " +
-                "are empty. Aborting.");
+        "are empty. Aborting.");
         return;
       }
 
@@ -73,8 +73,8 @@ export const syncTransactions = functions.
         // Sets the batch size (system max is 500)
         const batchSize = 300;
 
-        let earliestBlockWithUnconfirmed = -1;
-        let updatedBlockHeight = blockHeight;
+        let earliestBlockWithUnconfirmedTx = null;
+        let latestBlockWithConfirmedTx = null;
 
         // Find out the number of batch runs
         const remainder = relevantTxs.length % batchSize;
@@ -93,25 +93,37 @@ export const syncTransactions = functions.
             console.log(`Starting batch run #${run + 1} with ` +
             `transactions indexes from ${start} to ${end}.`);
 
-            const batchResult = await runBatch(relevantTxs, start, end,
-                updatedBlockHeight, earliestBlockWithUnconfirmed);
+            const batchResult = await runBatch(relevantTxs, start, end);
 
-            updatedBlockHeight = batchResult.updatedBlockHeight;
-            earliestBlockWithUnconfirmed = batchResult.firstUnconfirmedBlock;
+            earliestBlockWithUnconfirmedTx =
+            earliestBlockWithUnconfirmedTx ??
+            batchResult.earliestBlockWithUnconfirmedTx;
+
+            latestBlockWithConfirmedTx =
+            batchResult.latestBlockWithConfirmedTx ??
+            latestBlockWithConfirmedTx;
           }
+
+          const newBlockHeight = earliestBlockWithUnconfirmedTx ??
+          (latestBlockWithConfirmedTx ?
+          latestBlockWithConfirmedTx + 1 : blockHeight);
+
+          const lndSyncDoc = {
+            "block_height": newBlockHeight,
+            "timestamp": FieldValue.serverTimestamp(),
+          };
+          await configCollection.doc("lnd_sync").set(lndSyncDoc);
+          console.log(`Next sync block height: ${newBlockHeight}`);
         } else {
           console.log("As there are no transactions to sync, " +
-          "will just update last sync timestamp.");
+          "will just update last sync timestamp. " +
+          `Next sync block height: ${blockHeight}`);
           const lndSyncDoc = {
-            "block_height": updatedBlockHeight,
+            "block_height": blockHeight,
             "timestamp": FieldValue.serverTimestamp(),
           };
           await configCollection.doc("lnd_sync").set(lndSyncDoc);
         }
-
-
-        console.log("Finished. Updated block height " +
-        `for next sync: ${updatedBlockHeight}`);
       } else {
         throw new HttpsError("aborted",
             "Error when trying to activate lightning");
@@ -125,64 +137,43 @@ export const syncTransactions = functions.
  *    respective subcollection (incoming_txs) in the addresses colletion.
  * 2. Insert (or update)  outgoing transactions (payments) to their
  *    respective subcolletion (outgoing_txs) in the wallets collection.
- * 3. Updates the block height for the next batch.
  * @param {Array} transactions the full list of transactions
  * @param {int} start the index of the first transaction of this batch
  * @param {int} end the index of the last transaction of this batch
- * @param {int} lastConfirmedBlock the last block where there are
- *              confirmed transaction block,
- * @param {int} earliestBlockWithUnconfirmed the earliest block where
- *              there are unconfirmed transactions.
- * @return {Map} the updated lastConfirmedBlock and
- *               earliestBlockWithUnconfirmed to be used in the next
- *               batch.
+ * @return {Map} the latest block where confirmed transations were found and
+ *                the earliest block where unconfirmed transactions were found.
  */
-async function runBatch(transactions, start, end,
-    lastConfirmedBlock, earliestBlockWithUnconfirmed) {
-  console.log("Earliest block with unconfirmed transactions: " +
-    `${earliestBlockWithUnconfirmed ?
-      "None" : earliestBlockWithUnconfirmed}`);
+async function runBatch(transactions, start, end) {
+  console.log(`Processing batch from index ${start} to ${end}.`);
 
   const batch = db.batch();
+
+  let earliestBlockWithUnconfirmedTx = null;
+  let latestBlockWithConfirmedTx = null;
 
   for (let i = start; i < end; i++) {
     const transaction = transactions[i];
 
-    let result;
+    let txStatus;
     if (transaction.amount > 0) {
       // This is an incoming transaction
-      result = await processIncomingTransaction(batch, transaction,
-          lastConfirmedBlock,
-          earliestBlockWithUnconfirmed);
+      txStatus = await processIncomingTransaction(batch, transaction);
     } else if (transaction.amount < 0) {
       // This is an outgoing transaction (onchain payment)
-      result = await processOutgoingTransaction(batch, transaction,
-          lastConfirmedBlock,
-          earliestBlockWithUnconfirmed);
+      txStatus = await processOutgoingTransaction(batch, transaction);
     } else {
       console.log("Transaction with amount equal 0 was " +
         `found: ${transaction.tx_hash}`);
     }
-    if (result) {
-      lastConfirmedBlock = result.lastConfirmedBlock;
-      earliestBlockWithUnconfirmed = result.earliestBlockWithUnconfirmed;
+
+    if (txStatus == "CONFIRMED") {
+      latestBlockWithConfirmedTx = transaction["block_height"];
+    } else if (txStatus == "UNCONFIRMED") {
+      earliestBlockWithUnconfirmedTx =
+        earliestBlockWithUnconfirmedTx ??
+        transaction["block_height"];
     }
   }
-
-  /* If we have unconfirmed transactions, we should start the next sync
-     from the earliest block with them.
-     Otherwise, we should start from the block subsequent
-     to the last confirmed one. */
-  const updatedBlockHeight = earliestBlockWithUnconfirmed > 0 ?
-    earliestBlockWithUnconfirmed : lastConfirmedBlock + 1;
-  const lndSyncDoc = {
-    "block_height": updatedBlockHeight,
-    "timestamp": FieldValue.serverTimestamp(),
-  };
-  const lndSyncRef = configCollection.doc("lnd_sync");
-  batch.set(lndSyncRef, lndSyncDoc);
-
-  console.log(`Saved updated block height: ${updatedBlockHeight}`);
 
   // Finally commit all the changes to the database
   await batch.commit();
@@ -191,24 +182,17 @@ async function runBatch(transactions, start, end,
 
   // Returns the updated block height and the earliest block where unconfirmed
   // transactions were found (it may be used in the next batch run)
-  return {updatedBlockHeight, earliestBlockWithUnconfirmed};
+  return {earliestBlockWithUnconfirmedTx, latestBlockWithConfirmedTx};
 }
 
 /**
  * Process an incoming transaction
  * @param {*} batch the current batch being processed
  * @param {*} transaction the current transaction being processed
- * @param {*} lastConfirmedBlock the last block where there are
- *                               confirmed transactions
- * @param {*} earliestBlockWithUnconfirmed the earliest block where there
- *                                         are unconfirmed transactions
- * @return {Map} the updated lastConfirmedBlock and
- *               earliestBlockWithUnconfirmed to be used in the next
- *               batch.
+ * @return {Map} the latest block where confirmed transations were found and
+ *                the earliest block where unconfirmed transactions were found.
  */
-async function processIncomingTransaction(batch, transaction,
-    lastConfirmedBlock,
-    earliestBlockWithUnconfirmed) {
+async function processIncomingTransaction(batch, transaction) {
   const tx = {
     "address": transaction.address,
     "tx_hash": transaction.tx_hash,
@@ -222,29 +206,25 @@ async function processIncomingTransaction(batch, transaction,
   // We write it as an element of a subcollection inside the address document
   const incomingTxColRef = addressesCollection.doc(transaction.address).
       collection("incoming_txs");
+
+
   const txRef = incomingTxColRef.doc(transaction.tx_hash);
 
   // Writes the transaction to the firebase database.
   // Will update fields if is exists.
   batch.set(txRef, tx);
 
-  // Check if the transaction is confirmed. If not, it should be written
-  // to unconfirmed transactions collection
   if (transaction.num_confirmations < 6) {
-    earliestBlockWithUnconfirmed = earliestBlockWithUnconfirmed > 0 ?
-      earliestBlockWithUnconfirmed :
-      (transaction.block_height > 0 ?
-        transaction.block_height : -1);
     console.log(`Saved UNCONFIRMED incoming tx: ${transaction.tx_hash}. ` +
       `(${transaction.num_confirmations} confirmations).`);
-  } else {
-    lastConfirmedBlock = transaction.block_height;
-    console.log("INCOMING transaction is now CONFIRMED. Tx hash: " +
-      `${transaction.tx_hash}. (${transaction.num_confirmations} ` +
-      "confirmations).");
+    return "UNCONFIRMED";
   }
 
-  return {lastConfirmedBlock, earliestBlockWithUnconfirmed};
+  console.log(`INCOMING transaction to address ${transaction.address} ` +
+              `is now CONFIRMED. Tx hash: ${transaction.tx_hash}. ` +
+              `(${transaction.num_confirmations} confirmations).`);
+
+  return "CONFIRMED";
 }
 
 
@@ -252,17 +232,10 @@ async function processIncomingTransaction(batch, transaction,
  * Process an outgoing transaction
  * @param {*} batch the current batch being processed
  * @param {*} transaction the current transaction being processed
- * @param {*} lastConfirmedBlock the last block where there are
- *                               confirmed transactions
- * @param {*} earliestBlockWithUnconfirmed the earliest block where there
- *                                         are unconfirmed transactions
- * @return {Map} the updated lastConfirmedBlock and
- *               earliestBlockWithUnconfirmed to be used in the next
- *               batch.
+ * @return {Map} the latest block where confirmed transations were found and
+ *                the earliest block where unconfirmed transactions were found.
  */
-async function processOutgoingTransaction(batch, transaction,
-    lastConfirmedBlock,
-    earliestBlockWithUnconfirmed) {
+async function processOutgoingTransaction(batch, transaction) {
   // Get the transaction in the database
   const labelSplit = transaction.label.split(":");
   const walletId = labelSplit[1];
@@ -287,43 +260,38 @@ async function processOutgoingTransaction(batch, transaction,
       "txid": transaction.tx_hash,
     };
   }
-  const newStatus = transaction.num_confirmations < 6 ?
-    "UNCONFIRMED" : "CONFIRMED";
-  if (tx["status"] == "NEW" || tx["status"] != newStatus) {
-    tx["timestamp"] = Timestamp.fromMillis(transaction.time_stamp * 1000);
-    tx["fee"] = transaction.total_fees;
-    if (transaction.num_confirmations < 6) {
-      tx["status"] = "UNCONFIRMED";
-      if (transaction.block_height > 0) {
-        tx["block_height"] = transaction.block_height;
-        earliestBlockWithUnconfirmed = earliestBlockWithUnconfirmed > 0 ?
-          earliestBlockWithUnconfirmed : transaction.block_height;
-      }
-      // Writes the transaction to the firebase database.
-      // Will update fields if is exists.
-      batch.set(txRef, tx);
-      console.log(`Saved UNCONFIRMED outcoming tx id: ${paymentId}`);
-    } else {
-      tx["status"] = "CONFIRMED";
-      tx["block_height"] = transaction.block_height;
-      lastConfirmedBlock = transaction.block_height;
 
-      // Writes the transaction to the firebase database.
-      // Will update fields if is exists.
-      batch.set(txRef, tx);
-      console.log("OUTGOING transaction is now CONFIRMED. Payment id: " +
-        `${paymentId}. ${transaction.num_confirmations} ` +
-        `confirmations. Tx hash: ${transaction.tx_hash}.`);
+  tx["timestamp"] = Timestamp.fromMillis(transaction.time_stamp * 1000);
+  tx["fee"] = transaction.total_fees;
+  if (transaction.num_confirmations < 6) {
+    tx["status"] = "UNCONFIRMED";
+    let retStatus = "UNCONFIRMED";
+    if (transaction.block_height > 0) {
+      tx["block_height"] = transaction.block_height;
+    } else {
+      retStatus = "MEMPOOL";
     }
-  } else {
-    console.log("Found unconfirmed outcoming tx but with no changes. " +
-      `Payment id: ${paymentId}. ${transaction.num_confirmations} ` +
-      `confirmations. Tx hash: ${transaction.tx_hash}.`);
+    // Writes the transaction to the firebase database.
+    // Will update fields if is exists.
+    batch.set(txRef, tx);
+    console.log(`Saved ${retStatus} outgoing tx id: ${paymentId}`);
+
+    return retStatus;
   }
 
-  return {lastConfirmedBlock, earliestBlockWithUnconfirmed};
-}
+  tx["status"] = "CONFIRMED";
+  tx["block_height"] = transaction.block_height;
 
+  // Writes the transaction to the firebase database.
+  // Will update fields if is exists.
+  batch.set(txRef, tx);
+  console.log(`OUTGOING transaction from wallet '${walletId}' is now ` +
+              `CONFIRMED. Payment id: ${paymentId}. ` +
+              `${transaction.num_confirmations} confirmations. ` +
+              `Tx hash: ${transaction.tx_hash}.`);
+
+  return "CONFIRMED";
+}
 
 /**
  * Get the list of incoming addresses relevant to the database
